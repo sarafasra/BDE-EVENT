47aeefd4b156b372c0ed1176297d89a5c7940890
Sarafasra <sarafasraoui75@gmail.com>
2026-07-22 21:50:42 +0100

    Ajouter l'enregistrement des événements avec validation

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 use App\Models\Event;
+
 
 class EventController extends Controller
 {
@@ -15,5 +16,20 @@ class EventController extends Controller
     public function create()
 {
     return view('events.create');
+}
+
+
+public function store(Request $request){
+    $validate = $request->validate([
+       'title' => 'required|string|max:255',
+        'description' => 'required|string',
+        'date' => 'required|date',
+        'time' => 'required',
+        'location' => 'required|string|max:255',
+        'price' => 'required|numeric|min:0',
+        'capacity' => 'required|integer|min:1',
+    ]);
+    Event::create($validate);
+    return redirect('/events')->with('sucess', 'Evenement creer avec sucess');
 }
 }
