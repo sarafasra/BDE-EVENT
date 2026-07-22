55dd76d10baec44b60316f7c4b06dcc49068ea0e
Sarafasra <sarafasraoui75@gmail.com>
2026-07-22 11:33:32 +0100

    ajouter les routes des login et logout

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login.post');
+Route::post('/logout', [AuthController::class , 'logout'])->name('logout');
