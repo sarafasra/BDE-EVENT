fcc22049cc50fd3fa22380ac481617f149517e15
Sarafasra <sarafasraoui75@gmail.com>
2026-07-22 11:15:14 +0100

    Ajouter les seeders des utilisateurs

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
-    }
+   public function run(): void
+{
+
+    User::create([
+        'name' => 'Admin',
+        'email' => 'admin@bde.com',
+        'password' => Hash::make('password'),
+        'role' => 'admin',
+    ]);
+
+    User::create([
+        'name' => 'Student',
+        'email' => 'student@bde.com',
+        'password' => Hash::make('password'),
+        'role' => 'student',
+    ]);
+}
 }
