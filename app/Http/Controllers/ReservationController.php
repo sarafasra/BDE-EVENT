9278609903b7d0bf066d9e573ae667fab2bb4f14
Sarafasra <sarafasraoui75@gmail.com>
2026-07-23 10:34:52 +0100

    Ajouter la logique de réservation des événements

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Reservation;
+use Illuminate\Support\Str;
 
 class ReservationController extends Controller
 {
-    //
+    public function store(Event $event){
+        $reservation = Reservation::where('user_id', auth()->id())
+        ->where('event_id', $event->id)->first();
+
+        if($reservation){
+            return redirect('/events')->with('error', 'vous etes deja inscit à cet événement');
+        }
+
+        if($event->reservation()->count()>= $event->capacity){
+            return redirect('/events')->with('error', 'cet événement est complet');
+        }
+
+        Reservation::create([
+
+        'reservation_code' => 'BDE-2026-' .strtoupper(Str::random(5)),
+        'reservation_date' => now(),
+        'user_id' => auth()->id(),
+        'event_id' => $event->id,
+        ]);
+         return redirect('/events')->with('success', 'Reservation effectuée avec succés');
+    }
 }
